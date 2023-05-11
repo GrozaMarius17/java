2f6ef1f92056ee99b3aeddd7de7d7bf47af84c60
Groza Marius <mariusgroza520@gmail.com>
2023-05-11 13:47:30 +0300

    curs15HomeworkPalindrome,Fibonacci

--- src/Homewrok15/RecursionHomework.java
+++ src/Homewrok15/RecursionHomework.java
@@ -1,12 +1,17 @@
 package Homewrok15;
-import java.util.Scanner;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class RecursionHomework {
     public static void main(String[] args) {
+        Map<Long , Long> fibonacciResult = new HashMap<>();
+        System.out.println(fibonacciArray(10));
+        System.out.println(sumOfDigitsForANumber(12345));
         System.out.println(sumOfFirstIntegerNumbers(10));
         System.out.println(sumOfFirstEvenIntegersNumbers(13));
-        System.out.println(palindrome("raluca"));
+        palindrome1("ana");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the String for check:");
         String string = scanner.nextLine();
@@ -35,17 +40,16 @@ public class RecursionHomework {
         return n + sumOfFirstEvenIntegersNumbers(n - 2);
    }
 
-   public static boolean palindrome(String str){
-       int n = str.length();
-       boolean ok = true;
-       for(int i = 0 ; i <= (n - 1) / 2 ; i++){
-           if(str.charAt(i) != str.charAt(n - 1)){
-               ok = false;
-               return ok;
-           }
-           n = n -1;
+   public static void palindrome1(String str){
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--){
+            reverse = reverse + str.charAt(i);
        }
-       return ok;
+        if(str.equals(reverse)){
+            System.out.println("Is palindrome");
+        }else {
+            System.out.println("Is not palindrome");
+        }
    }
 
    public static boolean palindrome2(String str){
@@ -57,5 +61,22 @@ public class RecursionHomework {
        }
        return false;
    }
+
+   public static int sumOfDigitsForANumber(int number){
+       if ( number / 10 == 0) {
+           return number;
+       }
+       return number % 10 + sumOfDigitsForANumber(number / 10);
+   }
+
+    public static int fibonacciArray(int k){
+        if(k == 0){
+            return 0;
+        }
+        if(k == 1){
+            return 1;
+        }
+        return fibonacciArray(k-1) + fibonacciArray(k-2);
+    }
 }
 
